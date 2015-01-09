55e8f0d7e1fba909703956164822bfd01bf91826
Raphael Krut-Landau <raphael.kl@gmail.com>
2015-01-09 03:39:56 -0500

    hasty commit without summary

--- memrise-typing.user.js
+++ memrise-typing.user.js
@@ -1,56 +1,21 @@
 // ==UserScript==
-// @name           Memrise Make Me Type
-// @description    Converts all Memrise multiple choice tests to typing tests.
+// @name           Memrise: Make Me Type
+// @description    Death to Scantrons! This script converts Memrise
+//                 multiple-choice tests to typing tests, where possible.
 // @match          http://www.memrise.com/course/*/garden/*
 // @match          http://www.memrise.com/garden/water/*
-// @author         Raphael Krut-Landau
 // @grant          none
+// @run-at         document-start
 // ==/UserScript==
 
-function replaceChoicesWithTyping() {
-    if ($('.choices:visible').length > 0) {
-        $('.choices').hide();
-        $('.typing *').show();
-        $('.typing input').keypress(function (e) {
-            if (e.which == 13) { // enter pressed
-                answerGiven = $(this).val().trim();
-                var someChoiceMatched = false; // initial value
-                $('.choice').each(function () {
-                    var choiceText = $(this).find('.val').get(0).innerHTML;
-                    thisChoiceMatched = choiceText.indexOf(answerGiven) > -1;
-                    if (thisChoiceMatched) {
-                        someChoiceMatched = true;
-                        $(this).click();
-                    }
-                });
-                if (!someChoiceMatched) {
-                    $('.do-review').click();
-                }
-                $(this).val('');
-            }
-        });
-    }
-    window.setTimeout("replaceChoicesWithTyping();", 100);
+function convertMultipleChoiceToTyping(e) {
+    // Always jump to grow session level 6.
+    p = MEMRISE.garden.sessions.GrowSession.prototype.BoxFactory.prototype;
+    p.make_at_level_1 = p.make_at_level_6;
+    p.make_at_level_2 = p.make_at_level_6;
+    p.make_at_level_3 = p.make_at_level_6;
+    p.make_at_level_4 = p.make_at_level_6;
+    p.make_at_level_5 = p.make_at_level_6;
 }
 
-var onLoad = function($) {
-    replaceChoicesWithTyping();
-    MEMRISE.garden.box_types.MultipleChoiceBox = MEMRISE.garden.box_types.TypingTestBox;
-}
-
-var injectWithJQ = function(f) {
-    var script = document.createElement('script');
-    script.textContent = '$(' + f.toString() + '($));';
-    document.body.appendChild(script);
-};
-
-
-var inject = function(f) {
-    var script = document.createElement('script');
-    //script.textContent = '(' + f.toString() + ')();';
-    script.textContent = f.toString();
-    document.body.appendChild(script);
-};
-
-inject(replaceChoicesWithTyping);
-injectWithJQ(onLoad);
+document.addEventListener("afterscriptexecute", convertMultipleChoiceToTyping, true);
